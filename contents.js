09b1d1cd902b6e5c1facbd2d2f5cb076a7feaccc
SeanHarkin <32457182+SeanHarkin@users.noreply.github.com>
2018-06-14 15:22:58 -0400

    Repeating JSON update
    Didn't show in github after push from GitHub Desktop which apparently went through? Confused, but hopefully this works.

--- contents.js
+++ contents.js
@@ -60,10 +60,10 @@ let VSC = {
         "members": [{
             "h1": "Finlay Braithwaite",
             "h2": "Audio",
-            "p": "A juicer of a handy dude",
+            "p": "Finlay Braithwaite is an emerging digital designer. Leveraging his passion and professional experience in audio production, Finlay endeavours to create a positionally aware microphone system. To achieve this, Finlay is actively engaging and exploring wearable technology, motion tracking, digital signal processing, and the principles of industrial design. Through his work, Finlay hopes to eke the benchmark of perceived quality of recorded audio higher. Finlay's current and past practice focuses on sound production, design, and engineering for film, television, music, interactive, and installation. Recently, Finlay has focused on documentary film audio editorial & mixing. As Sound Supervisor, Finlay played a critical role in the Hugh Gibson's acclaimed documentary The Stairs, recipient of Canada's largest prize for the arts, the Toronto Film Critics Association award for Best Canadian Film. Finlay also practices as a composer, musician, and performer. Most notably, Finlay was an anchor member of the experimental avant garde recording and performance group Gravity Wave.",
             "text-align": "left",
             "left": "13%",
-            "text-color": "#cba311",
+            "text-color":"rgb(146, 53, 39)",
             "background": {
                 "source": "url(finlay.png)"
             },
@@ -79,7 +79,6 @@ let VSC = {
             "p": "Alex Kurina is an award-winning designer, filmmaker and media artist from Toronto, Canada. He holds a BFA from Ryerson University’s New Media program where he currently teaches. He has worked for a number of international commercial clients in various creative capacities and has had work exhibited at the Montreal Museum of Fine Art, InterAccess (Toronto), The Guggenheim (New York) and Eyebeam (New York).",
             "text-align": "right",
             "left": "45%",
-            "text-color": "#c34c12",
             "background": {
                 "source": "url(alex.png)"
             },
@@ -92,42 +91,40 @@ let VSC = {
         }, {
             "h1": "Zach Cox",
             "h2": "Colour",
-            "p": "What else can be said?",
+            "p": "Zachary has worked in the Film and Television industry for 15+ years, sharing his diverse creative talents as a cinematographer, colourist and educator. He has work screened at the AGO, Cannes, Sundance, SXSW, TIFF, VIFF and numerous other international festivals. Zach is also an Adjunct Professor at Ryerson University's RTA School of Media where he teaches undergraduate and graduate level video theory and production. He also teaches workshops for the NHL PA, YouTube Canada, and has guest lectured at Northeastern University.",
             "text-align": "left",
             "left": "13%",
-            "text-color": "#c34c12",
             "background": {
                 "source": "url(zach.png)"
             },
             "logo": {
                 "rotate": "rotate(.44turn)",
                 "top": "50%",
-                "left": "174%",
+                "left": "-60%",
                 "width": "300%"
             },
         }, {
             "h1": "Andres Landau",
             "h2": "Edit",
-            "p": "What else can be said?",
+            "p": "With over 15 years experience, Argentina-native Andres Landau is an award-winning editor and respected post-production supervisor in Toronto’s film and television community. In 2011, he oversaw post-production for The National Parks Project, and edited Sirmilik, the 2012 Genie Award winning short documentary by director Zacharias Kunuk. He recently edited Charles Officer’s feature documentary, Unarmed Verses produced by The National Film Board of Canada, Winner of the 2017 HotDocs Best Canadian Feature Documentary, 2017 Vancouver International Film Festival and 2018 TIFF Canada’s Top Ten Audience Award and The Stairs by Hugh Gibson, Winner of the 2016 Toronto Film Critics Association for Best Canadian Film. Current clients include Google, Vice and the Toronto International Film Festival. Selected credits include The Road to Webequie, Mission Asteroid, Dead of Winter, Heritage Minute: Richard Pierpoint, I Met the Walrus, The Dreaming, Journey to Christmas and Rites of Passage.",
             "text-align": "right",
             "left": "45%",
-            "text-color": "#824549",
             "background": {
                 "source": "url(andres.png)"
             },
             "logo": {
                 "rotate": "rotate(10turn)",
-                "top": "230%",
-                "left": "50%",
+                "top": "50%",
+                "left": "-60%",
                 "width": "300%"
             },
         }, {
             "h1": "Ryan V. Hays",
             "h2": "Composoting",
-            "p": "What else can be said?",
+            "p": "Computer art! Remainders! Gravity Wave! That TV show! Frisbee!",
             "text-align": "left",
             "left": "13%",
-            "text-color": "black",
+            "text-color":"black",
             "background": {
                 "source": "url(ryan.png)"
             },
@@ -140,11 +137,10 @@ let VSC = {
         }, {
             "h1": "Mike McDougall",
             "h2": "3D",
-            "p": "What else can be said?",
+            "p": "Mike would like to pursue my love of art and design in this forum while offering creative, design-driven solutions for Film, Broadcast, Commercial and Entertainment industries. With a strong interest and background in contemporary art and cultural theory, Mike believes he brings a fresh perspective to problem solving and creative visualising. He loves to be involved in projects at any stage, whether it be creation, direction or completion.",
             "text-align": "right",
             "left": "45%",
-            "text-color": "#cba311",
-            "background": {
+            "text-color":"#cba311","background": {
                 "source": "url(mike.png)"
             },
             "logo": {
