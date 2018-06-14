e4affd8868df46dbdfeb22280aef7e586aaab70e
Finlay Braithwaite <3164558@student.ocadu.ca>
2018-06-14 10:03:14 -0400

    added content

---
+++ contents.js
@@ -0,0 +1,104 @@
+/*let VSC = {
+    "Victory Social Club": [{
+        "places": [{
+            "h1": "colour",
+            "h2": "Grading Room",
+            "p": "This is a <b>fun</b> room"
+        }, {
+            "h1": "Sound",
+            "h2": "Audio Room",
+            "p": "This is a <b>sad</b> room"
+        }, {
+            "h1": "Edit",
+            "h2": "Grading Room",
+            "p": "This is an <b>important</b> room"
+        }],
+        "members": [{
+            "h1": "Finlay Braithwaite",
+            "h2": "Master of Design",
+            "p": "A juicer of a handy dude",
+            "background": {
+                "source": "url(Finlay.png)",
+                "top": "0%",
+                "left": "0%",
+                "width": "0%"
+            },
+        }, {
+            "h1": "Alex Kurina",
+            "h2": "Great Person",
+            "p": "What else can be said?",
+            "background": {
+                "source": "url(Colour.png)",
+                "top": "0%",
+                "left": "0%",
+                "width": "0%"
+            },
+        }, {
+            "h1": "Tony! Toni! Tone!",
+            "h2": "Three Tonys!",
+            "p": "What!!!!! HI HI HI HI HI"
+        }]
+    }]
+}
+*/
+
+let VSC = {
+    "Victory Social Club": [{
+        "places": [{
+            "h1": "colour",
+            "h2": "Grading Room",
+            "p": "This is a <b>fun</b> room"
+        }, {
+            "h1": "Sound",
+            "h2": "Audio Room",
+            "p": "This is a <b>sad</b> room"
+        }, {
+            "h1": "Edit",
+            "h2": "Grading Room",
+            "p": "This is an <b>important</b> room"
+        }],
+        "members": [{
+            "h1": "Finlay Braithwaite",
+            "h2": "Audio",
+            "p": "A juicer of a handy dude",
+            "background": {
+                "source": "url(Englebert.png)"
+            },
+        }, {
+            "h1": "Alex Kurina",
+            "h2": "Design",
+            "p": "Alex Kurina is an award-winning designer, filmmaker and media artist from Toronto, Canada. He holds a BFA from Ryerson University’s New Media program where he currently teaches. He has worked for a number of international commercial clients in various creative capacities and has had work exhibited at the Montreal Museum of Fine Art, InterAccess (Toronto), The Guggenheim (New York) and Eyebeam (New York).",
+            "background": {
+                "source": "url(Colour.png)"
+            },
+        }, {
+            "h1": "Zach Cox",
+            "h2": "Colour",
+            "p": "What else can be said?",
+            "background": {
+                "source": "url(Colour.png)"
+            },
+        }, {
+            "h1": "Andres Landau",
+            "h2": "Edit",
+            "p": "What else can be said?",
+            "background": {
+                "source": "url(Colour.png)"
+            },
+        }, {
+            "h1": "Ryan V. Hays",
+            "h2": "Composoting",
+            "p": "What else can be said?",
+            "background": {
+                "source": "url(Colour.png)"
+            },
+        }, {
+            "h1": "Mike McDougall",
+            "h2": "3D",
+            "p": "What else can be said?",
+            "background": {
+                "source": "url(Colour.png)"
+            },
+        }]
+    }]
+}
